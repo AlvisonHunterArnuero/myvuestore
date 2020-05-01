55e9939c371512d506d74e72da4baf6261b8ffd5
Alvison Hunter <alvisonhunter@Alvisons-MacBook-Pro.local>
2020-05-01 12:57:00 -0600

    added card payment form

---
+++ src/components/Payment.vue
@@ -0,0 +1,62 @@
+<template>
+  <form>
+    <div class="form-row">
+      <div class="form-group col-md-6">
+        <label for="inputEmail4">Card Holder Name</label>
+        <input type="email" class="form-control" id="inputEmail4" />
+      </div>
+      <div class="form-group col-md-6">
+        <label for="inputPassword4">Card Number</label>
+        <input type="password" class="form-control" id="inputPassword4" />
+      </div>
+    </div>
+    <div class="form-group">
+      <label for="expMonth">Expiration Month</label>
+      <select id="expMonth" class="form-control">
+        <option selected>Choose...</option>
+        <option value="January">January</option>
+        <option value="Febuary">Febuary</option>
+        <option value="March">March</option>
+        <option value="April">April</option>
+        <option value="May">May</option>
+        <option value="June">June</option>
+        <option value="July">July</option>
+        <option value="August">August</option>
+        <option value="September">September</option>
+        <option value="October">October</option>
+        <option value="November">November</option>
+        <option value="December">December</option>
+      </select>
+    </div>
+    <div class="form-group">
+      <label for="expYear">Expiration Year</label>
+      <input type="text" class="form-control" id="expYear" />
+    </div>
+
+    <div class="form-group">
+      <label for="cvv">Card Verification Value</label>
+      <input type="text" class="form-control" id="cvv" />
+    </div>
+
+    <button type="submit" class="btn btn-primary">Continue to Checkout</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "Payment",
+
+  data() {
+    return {
+      name: "",
+    };
+  },
+
+  methods: {
+    resolve_img_url: function(path) {
+      let images = require.context("../assets/", false, /\.png$|\.jpg$/);
+      return images("./" + path);
+    },
+  },
+};
+</script>
--- src/pages/Checkout.vue
+++ src/pages/Checkout.vue
@@ -1,38 +1,53 @@
 <template>
   <div class="container-fluid margin-bottom">
-    <div class="row justify-content-center">
-      <h3 class="display-3 m-4 text-info">Shopping Cart Checkout</h3>
-    </div>
-    <div class="row">
-      <!-- start table  -->
-      <b-table
-        :sticky-header="stickyHeader"
-        hover
-        responsive
-        head-variant="dark"
-        :items="cartAddedItems"
-        :fields="fields"
-      >
-        <template v-slot:cell(img)="data">
-          <b-img
-            :src="resolve_img_url(data.value)"
-            fluid
-            alt="pic"
-            class="thumb"
-          ></b-img>
-        </template>
-      </b-table>
-    </div>
-    <!-- end table -->
-
-    <div class="row justify-content-between">
+    <div class="row justify-content-between py-3">
+      <div class="col-auto">
+        <h1 class="text-info text-uppercase">
+          Shopping Cart Checkout
+        </h1>
+      </div>
       <div class="col-auto">
         <router-link to="/page1">
-          <button type="button" class="btn btn-info btn-lg mb-3">
-            <font-awesome-icon icon="home" />Back to Home
+          <button type="button" class="btn btn-outline-info btn-lg">
+            <font-awesome-icon icon="home" />
           </button>
         </router-link>
       </div>
+    </div>
+    <div class="row">
+      <!-- start table  -->
+      <div class="col-12 col-sm-12 col-md-12 col-lg-8">
+        <b-table
+          :sticky-header="stickyHeader"
+          hover
+          head-variant="light"
+          :items="cartAddedItems"
+          :fields="fields"
+          responsive="sm"
+        >
+          <template v-slot:cell(img)="data">
+            <b-img
+              :src="resolve_img_url(data.value)"
+              fluid
+              alt="pic"
+              class="thumb"
+            ></b-img>
+          </template>
+        </b-table>
+      </div>
+      <div class="col-12 col-sm-12 col-md-12 col-lg-4">
+        <b-card
+          header="Payment Details"
+          footer="Card Footer"
+          title="We accept all major cards."
+        >
+          <Payment />
+        </b-card>
+      </div>
+    </div>
+    <!-- end table -->
+
+    <div class="row justify-content-end border-top border-secondary mt-2 pt-3">
       <div class="col-auto">
         <h4 class="text-primary text-uppercase">
           Sub-Total:
@@ -61,8 +76,12 @@
 
 <script>
 import { mapState } from "vuex";
+import Payment from "../components/Payment";
 export default {
-  name: "Product",
+  name: "Checkout",
+  components: {
+    Payment,
+  },
   data() {
     return {
       userId: "",
