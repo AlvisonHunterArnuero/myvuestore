baf2858e953799de0ce952cedf9939f76a2d69c8
Alvison Hunter <alvisonhunter@Alvisons-MacBook-Pro.local>
2020-05-07 13:24:09 -0600

    refreshing build folder

--- src/stockItems/stockItems.js
+++ src/stockItems/stockItems.js
@@ -1,45 +1,45 @@
 export const stockItems = [
   {
-    itemName: "Wrangler Men’s Twill Cargo Short",
+    itemName: "Wrangler Twill Cargo Short",
     itemType: "Short",
     price: "$18.53",
-    description: "100% Cotton - Imported - Zipper closure - Machine Wash",
+    description: "100% Cotton - Zipper closure - Machine Wash",
     imgURL: "./assets/shorts/short_cream.jpg",
   },
   {
-    itemName: "Wrangler Men’s Twill Cargo Short",
+    itemName: "Wrangler Twill Cargo Short",
     itemType: "Short",
     price: "$72.90",
-    description: "100% Cotton - Imported - Zipper closure - Machine Wash",
+    description: "100% Cotton - Zipper closure - Machine Wash",
     imgURL: "./assets/shorts/short_squares.jpg",
   },
   {
-    itemName: "Wrangler Men’s Twill Cargo Short",
+    itemName: "Wrangler Twill Cargo Short",
     itemType: "Short",
     price: "$19.09",
-    description: "100% Cotton - Imported - Zipper closure - Machine Wash",
+    description: "100% Cotton - Zipper closure - Machine Wash",
     imgURL: "./assets/shorts/short_camouflage.jpg",
   },
   {
-    itemName: "Unionbay Men's Cordova Belted Messenger Cargo Short",
+    itemName: "Unionbay Belted Cargo Short",
     itemType: "Short",
     price: "$23.81",
-    description: "100% Cotton - Imported - Zipper closure - Machine Wash",
+    description: "100% Cotton - Zipper closure - Machine Wash",
     imgURL: "./assets/shorts/short_gray.jpg",
   },
   {
-    itemName: "LEE Men's Dungarees New Belted Wyoming Cargo Short",
+    itemName: "LEE New Belted Wyoming Cargo Short",
     itemType: "Short",
     price: "$9.88",
     description:
-      "60% Cotton/40% Polyester -Imported -Machine Wash -X-Temp, a dynamic moisture control technology",
+      "60% Cotton/40% Polyester - Machine Wash",
     imgURL: "./assets/shorts/short_squares.jpg",
   },
   {
     itemName: "Hanes Men's 2 Pack X-Temp Performance T-Shirt",
     itemType: "Tshirt",
     price: "$12.99",
-    description: "100% Cotton - Imported - Zipper closure - Machine Wash",
+    description: "100% Cotton - Zipper closure - Machine Wash",
     imgURL: "./assets/shorts/tshirt_blue.jpg",
   },
   {
@@ -51,44 +51,44 @@ export const stockItems = [
   },
   {
     itemName:
-      "Columbia Women's Arcadia Ii Waterproof Breathable Jacket with Packable Hood",
+      "Columbia Waterproof Jacket with Packable Hood",
     itemType: "Jacket",
     price: "$44.88",
     description:
-      "Shell: 100% Nylon; Lining: 100% Polyester - Imported - Zipper closure - Machine Wash",
+      "100% Nylon; Lining: 100% Polyester - Machine Wash",
     imgURL: "./assets/shorts/jacket_black.jpg",
   },
   {
-    itemName: "YunJey Round Neck Triple Color Block Stripe T-Shirt",
+    itemName: "YunJey Stripe T-Shirt",
     itemType: "Tshirt",
     price: "$16.99",
     description:
-      "Line Dry - Classic summer striped shirt with short sleeves - Simple, chic and delightful top for summer,you can pair it with jeans or leggings",
-    imgURL: "./assets/shorts/tshirt_stripes.jpg",
+      "Classic summer striped short sleeves shirt",
+    imgURL: "./assets/shorts/tshirt_stripes_.jpg",
   },
   {
     itemName:
-      "DB MOON Women Summer Casual Short Sleeve Dresses Empire Waist Dress with Pockets",
+      "DB MOON Empire Waist Dress",
     itemType: "Dress",
     price: "$26.99",
     description:
-      "95% rayon 5% spandex - 0.7 high - 14 wide - Fabric Material: 95% Rayon",
+      "95% rayon 5% spandex - Fabric Material: 95% Rayon",
     imgURL: "./assets/shorts/empire_waist_midi_dress.jpg",
   },
 
   {
     itemName:
-      "PALINDA Women's Striped Elegant Short Sleeve Wear to Work Casual Pencil Dress with Belt",
+      "PALINDA Short Sleeve Pencil Dress",
     itemType: "Dress",
     price: "$23.99",
     description:
-      "Stripe Printed, Short Sleeve, Scoop Neck, Side-seam Pockets, knee length, Business Bodycon Pencil Dress, Office Midi Dress with Belt.",
+      "Stripe Printed, Short Sleeve, Office Midi Dress with Belt.",
     imgURL: "./assets/shorts/dress_stripes.jpg",
   },
 
   {
     itemName:
-      "Fengbay High Waist Yoga Pants, Pocket Yoga Pants Tummy Control Workout Running 4 Way Stretch Yoga Leggings",
+      "Fengbay Stretch Yoga Leggings",
     itemType: "leggins",
     price: "$21.99",
     description: "100% Cotton - Imported - Machine Wash",
@@ -96,126 +96,126 @@ export const stockItems = [
   },
 
   {
-    itemName: "Amazon Essentials Women's Tank Maxi Dress",
+    itemName: "Essentials Tank Maxi Dress",
     itemType: "Dress",
     price: "$26.00",
     description:
-      "95% Rayon, 5% Elastane - Imported - Machine Wash - Stylish tank style sleeveless maxi dress perfect for a casual day out",
+      "95% Rayon, 5% Elastane - Imported - Machine Wash",
     imgURL: "./assets/shorts/maxi_dress.jpg",
   },
 
   {
     itemName:
-      "SheIn Women's V Neck Spaghetti Straps Sleeveless Sexy Backless Wrap Flare Dress",
+      "SheIn Sleeveless Wrap Flare Dress",
     itemType: "Dress",
     price: "$14.99",
     description:
-      "95% polyester , 5% spandex - Pull On closure -v95% Polyester, 5% Spandex, fabric has some stretch Dress",
+      "95% polyester , 5% spandex - Pull On closure",
     imgURL: "./assets/shorts/flare_dress_.jpg",
   },
 
   {
     itemName:
-      "Amazon Brand - Daily Ritual Women's Tencel long-Sleeve Shirt Dress ",
+      "Tencel long-Sleeve Shirt Dress ",
     itemType: "dress",
     price: "$35.96",
-    description: "100% Lyocell - Imported -Button closure - Machine Wash",
+    description: "100% Lyocell - Button closure - Machine Wash",
     imgURL: "./assets/shorts/tencel_long_sleeve_shirt_dress_.jpg",
   },
   {
     itemName:
-      "Amazon Brand - Daily Ritual Women's Jersey Short-Sleeve V-Neck T-Shirt Dress",
+      "Short-Sleeve V-Neck T-Shirt Dress",
     itemType: "dress",
     price: "$18.50",
     description:
-      "95% Rayon, 5% Spandex - Imported - Machine Wash - This essential t-shirt dress features a V-neck and a relaxed fit for an effortless look that's ready to style",
+      "95% Rayon, 5% Spandex - Imported - Machine Wash",
     imgURL: "./assets/shorts/v-neck_tshirt_dress.jpg",
   },
 
   {
     itemName:
-      "Amazon Brand - Daily Ritual Women's Lived-in Cotton Short-Sleeve Crewneck Maxi Dress",
+      "Short-Sleeve Crewneck Maxi Dress",
     itemType: "dress",
     price: "$15.50",
     description:
-      "100% Cotton Imported - Machine Wash - This soft, smooth, and lightweight maxi dress is exceptionally versatile and flattering.",
+      "100% Cotton Machine Wash - This soft, smooth, and lightweight.",
     imgURL: "./assets/shorts/crew_neck_maxi_dress.jpg",
   },
 
   {
     itemName:
-      "Amazon Brand - Daily Ritual Women's Jersey Sleeveless Empire-Waist Midi Dress",
+      "Sleeveless Empire-Waist Midi Dress",
     itemType: "dress",
     price: "$12.14",
-    description: "95% Viscose, 5% Elastane - Imported - Machine Wash",
+    description: "95% Viscose, 5% Elastane - Machine Wash",
     imgURL: "./assets/shorts/sleeveless_empire_waist_midi_dress.jpg",
   },
 
   {
     itemName:
-      "Amazon Brand - Daily Ritual Women's Jersey Short-Sleeve Crewneck Boxy Pocket T-Shirt Dress",
+      "Short-Sleeve Crewneck T-Shirt Dress",
     itemType: "dress",
     price: "$11.80",
     description:
-      "95% Viscose, 5% Elastane - Imported - Machine Wash - Made in Vietnam",
+      "95% Viscose, 5% Elastane - Machine Wash - Made in Vietnam",
     imgURL: "./assets/shorts/boxy_pocket_tshirt_dress.jpg",
   },
   {
     itemName:
-      "Amazon Brand - Daily Ritual Women's Jersey Sleeveless V-Neck Midi Dress",
+      "Sleeveless V-Neck Midi Dress",
     itemType: "dress",
     price: "$19.43",
-    description: "95% Viscose, 5% Elastane - Imported - Machine Wash",
+    description: "95% Viscose, 5% Elastane - Machine Wash",
     imgURL: "./assets/shorts/v_neck_midi_dress.jpg",
   },
 
   {
-    itemName: "Amazon Brand - Daily Ritual Women's Jersey Muscle Swing Dress",
+    itemName: "Muscle Swing Dress",
     itemType: "Dress",
     price: "$19.02",
     description:
-      "95% Rayon, 5% Spandex Imported - Machine Wash - Made in Vietnam",
+      "95% Rayon, 5% Spandex Machine Wash - Made in Vietnam",
     imgURL: "./assets/shorts/swing_dress.jpg",
   },
 
   {
-    itemName: "Hanes Men's Long Sleeve Beefy Henley Shirt",
+    itemName: "Hanes Long Sleeve Beefy Henley Shirt",
     itemType: "shirt",
     price: "$22.51",
     description:
-      "100% Cotton; Heather: 75% Cotton/25% Polyester - Imported - Button closure - Machine Wash",
+      "100% Cotton & Polyester - Button closure - Machine Wash",
     imgURL: "./assets/shorts/henley_shirt.jpg",
   },
   {
-    itemName: "Carhartt Men's Workwear Pocket Henley Shirt",
+    itemName: "Carhartt Pocket Henley Shirt",
     itemType: "shirt",
     price: "$15.97",
     description:
-      "100% Cotton - Imported - Button closure - Carhartt Company Gear Collection",
+      "100% Cotton - Imported - Button closure",
     imgURL: "./assets/shorts/pocket_henley_shirt.jpg",
   },
   // here
   {
-    itemName: "Champion Men's MIDDLEWEIGHT Hoodie",
+    itemName: "Champion Men's Hoodie",
     itemType: "sweater",
     price: "$21.00",
-    description: "100% Cotton - Imported - Pull On closure - Machine Wash",
+    description: "100% Cotton - Pull On closure - Machine Wash",
     imgURL: "./assets/shorts/hoodie_sweater_black.jpg",
   },
 
   {
-    itemName: "Volcom Men's Crisp Stone Short Sleeve Tee",
+    itemName: "Volcom Men's Crisp Short Sleeve Tee",
     itemType: "Tshirt",
     price: "$11.99",
-    description: "100% Cotton - Imported - Button closure - Machine Wash",
+    description: "100% Cotton - Button closure - Machine Wash",
     imgURL: "./assets/shorts/short_sleeve_black_tshirt.jpg",
   },
 
   {
-    itemName: "Carhartt Men's Workwear Jersey Pocket Long-Sleeve Shirt K126",
+    itemName: "Carhartt Jersey Long-Sleeve Shirt",
     itemType: "shirt",
     price: "$18.04",
-    description: "100% Cotton - Imported - Adjustable closure - Machine Wash",
+    description: "100% Cotton - Adjustable closure - Machine Wash",
     imgURL: "./assets/shorts/long_sleeve_shirt.jpg",
   },
 ];
--- src/users/users.js
+++ src/users/users.js
@@ -1,7 +1,7 @@
 export const registeredUsers = [
   {
     username: "alvisonhunter",
-    password: "p",
+    password: "password2020",
     email: "alvisonhunter@alondelistore.com",
   },
   {
@@ -10,8 +10,23 @@ export const registeredUsers = [
     email: "ernestogutierrez@alondelistore.com",
   },
   {
-    username: "joedoe",
+    username: "luisguido",
+    password: "lagc2020",
+    email: "luisguido@alondelistore.com",
+  },
+  {
+    username: "oliverqueen",
     password: "12345678",
-    email: "joedoe@alondelistore.com",
+    email: "oliverqueen@alondelistore.com",
+  },
+  {
+    username: "blackcanary",
+    password: "12345678",
+    email: "blackcanary@alondelistore.com",
+  },
+  {
+    username: "johndiggle",
+    password: "12345678",
+    email: "johndiggle@alondelistore.com",
   },
 ];
